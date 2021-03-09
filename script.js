629cfda41b0ec3a9a471b9bcedb1ff07192177cf
Tom Cyparski <t.cyparski@gmail.com>
2021-03-09 22:22:54 +0000

    css update

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val; 
+        }
         
