0610978f7e8cf37b6182c1a1254f2fd768d44771
Tom Cyparski <t.cyparski@gmail.com>
2021-03-11 18:55:44 +0000

    js update

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function compute()
-{
+function compute() {
     p = document.getElementById("principal").value;
     r = document.getElementById("rangeInput").value;
     y = document.getElementById("years").value;
     document.getElementById("result").textContent = p * r/100 * y;
-    
+    return false;
 }
 
 function updateTextInput(val) {
