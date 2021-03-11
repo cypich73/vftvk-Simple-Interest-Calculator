dab8c545ca7e9c7913f7296cdcb8ca804c68cbb2
Tom Cyparski <t.cyparski@gmail.com>
2021-03-11 18:24:51 +0000

    js update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     p = document.getElementById("principal").value;
     r = document.getElementById("rangeInput").value;
     y = document.getElementById("years").value;
-    document.getElementById("result").innerHTML = p * r/100 * y;
+    document.getElementById("result").textContent = p * r/100 * y;
     
 }
 
