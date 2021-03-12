074ace164cf5fe40fb7167d8baccfbfedc85d1f6
Tom Cyparski <t.cyparski@gmail.com>
2021-03-12 22:54:22 +0000

    update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function compute() {
     event.preventDefault();
+
+    document.getElementById("principal_text").innerHTML="If you deposit ";
+    
     p = document.getElementById("principal").value;
     r = document.getElementById("rangeInput").value;
     y = document.getElementById("years").value;
     document.getElementById("result").innerHTML = p * r/100 * y;
 
-    document.getElementById("principal_text").innerHTML="If you deposit ";
+    
     //return false;
 }
 
