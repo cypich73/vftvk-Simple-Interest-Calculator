c039785e6205266ab1eab010285f0e5068ca8d14
Tom Cyparski <t.cyparski@gmail.com>
2021-03-13 19:42:39 +0000

    update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute() {
     r = document.getElementById("rangeInput").value;
     y = document.getElementById("years").value;
     document.getElementById("result").innerHTML = p * r/100 * y;
-    document.getElementById("result_years").innerHTML = 2020 * y;
+    document.getElementById("result_years").innerHTML = 2020 + y;
 
     //var z = "2020";
     //var x = parseInt(z);
@@ -17,7 +17,8 @@ function compute() {
     document.getElementById("principal_text").innerHTML="If you deposit " + p + ",";
     document.getElementById("rangeInput_text").innerHTML="at an interest rate of " + r + "%.";
     document.getElementById("result_text").innerHTML="You will receive the amount of ";
-    document.getElementById("years_text").innerHTML="in the year " + y;
+    document.getElementById("result_comma").innerHTML=",";
+    document.getElementById("years_text").innerHTML="in the year ";
 
 }
 
