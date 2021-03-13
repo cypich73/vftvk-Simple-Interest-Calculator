a0d9266715779f9d6c455b4629080ffdc6722c08
Tom Cyparski <t.cyparski@gmail.com>
2021-03-13 16:47:02 +0000

    update

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 function compute() {
     event.preventDefault();
-
     
     
-    document.getElementById("principal_comma").innerHTML=",";
-
-    document.getElementById("rangeInput_text").innerHTML="at an interest rate of ";
-    //document.getElementById("rangeInput").innerHTML= r;
-    document.getElementById("rangeInput_comma").innerHTML="%.";
-
-    document.getElementById("result_text").innerHTML="You will receive the amount of ";
     
-    document.getElementById("result_comma").innerHTML=",";
-    
-    document.getElementById("years_text").innerHTML="in the year ";
-    
-
-    //document.getElementById("principal").innerHTML= p;
     p = document.getElementById("principal").value;
     r = document.getElementById("rangeInput").value;
     y = document.getElementById("years").value;
     document.getElementById("result").innerHTML = p * r/100 * y;
 
-    document.getElementById("principal_text").innerHTML="If you deposit " + p;
+    document.getElementById("principal_text").innerHTML="If you deposit " + p + ",";
+    document.getElementById("rangeInput_text").innerHTML="at an interest rate of " + r + "%.";
+    document.getElementById("result_text").innerHTML="You will receive the amount of ";
+    document.getElementById("years_text").innerHTML="in the year " + y;
 
-    //return false;
+
 }
 
 // function updateTextInput(val) {
