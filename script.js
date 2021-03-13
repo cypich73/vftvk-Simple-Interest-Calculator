0f9fdc3bc6028805b3930511299f4bb30f21e8c1
Tom Cyparski <t.cyparski@gmail.com>
2021-03-13 16:22:12 +0000

    update

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ function compute() {
     event.preventDefault();
 
     document.getElementById("principal_text").innerHTML="If you deposit ";
-    var p = document.getElementById("principal").value;
+    
     document.getElementById("principal").innerHTML= p;
     document.getElementById("principal_comma").innerHTML=",";
 
     document.getElementById("rangeInput_text").innerHTML="at an interest rate of ";
-    r = document.getElementById("rangeInput").value;
+    
     document.getElementById("rangeInput").innerHTML= r;
     document.getElementById("rangeInput_comma").innerHTML="%.";
 
     document.getElementById("result_text").innerHTML="You will receive the amount of ";
-    document.getElementById("result").innerHTML = p * r/100 * y;
+    
     document.getElementById("result_comma").innerHTML=",";
     
     document.getElementById("years_text").innerHTML="in the year ";
-    y = document.getElementById("years").value;
+    
 
     
+    p = document.getElementById("principal").value;
+    r = document.getElementById("rangeInput").value;
+    y = document.getElementById("years").value;
+    document.getElementById("result").innerHTML = p * r/100 * y;
+
     //return false;
 }
 
