468ab93f826f1bbfa9bf967de8ea9f0b68521518
Tom Cyparski <t.cyparski@gmail.com>
2021-03-15 21:56:08 +0000

    update

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ function compute() {
 
 function checkNegativeValue() {
   negnum = parseFloat(document.getElementById("principal").value);
-  if (negnum < 0 || negnum == 0)
-  {
+  if (negnum < 0 || negnum == 0) {
    alert("Enter a positive number");
    return false;
-   clearInputs();
-   
   }
+   clearInputs();
+  
+  
 }
 
 
